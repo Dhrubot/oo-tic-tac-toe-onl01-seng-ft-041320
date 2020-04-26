90712743826c4c84d0b4dfb42b8ced27152a24a9
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-26 02:37:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,8 +71,7 @@ class TicTacToe
       elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
         return win_combo
       end
-        false
-      end
+      false
     end
   end
   
