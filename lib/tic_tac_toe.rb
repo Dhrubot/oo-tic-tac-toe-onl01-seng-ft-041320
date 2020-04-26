69c0f82d0a141f3e2d4b1db8c997bde3b11a75f4
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-26 00:58:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,12 @@
 class TicTacToe
   attr_accessor :board
   
-  WIN_COMBINATIONS = [[0, 1, 2],
-  
-    ]
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    []
+  ]
   def initialize
     @board = Array.new(9, " ")
   end
